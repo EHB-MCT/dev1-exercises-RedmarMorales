0627f67a1ea3067503933704d909b7f8213b9fed
Redmar Morales <redmarfrancis@yahoo.com>
2022-10-12 14:32:10 +0200

    exercise gemaakt

--- exercises/2-variables/2-1-more-lines.js
+++ exercises/2-variables/2-1-more-lines.js
@@ -8,10 +8,23 @@ let context = canvas.getContext('2d');
 let width = context.canvas.width;
 let height = context.canvas.height;
 
+let margin = 700;
+
 drawLines();
 
 function drawLines() {
+    context.lineWidth = 2;
     context.beginPath();
-
+    context.moveTo(margin, 50);
+    context.lineTo(width - margin, 50);
+    context.lineTo(margin, 100);
+    context.lineTo(width - margin, 100);
+    context.lineTo(margin, 150);
+    context.lineTo(width - margin, 150);
+    context.lineTo(margin, 200);
+    context.lineTo(width - margin, 200);
+    context.lineTo(margin, 250);
+    context.lineTo(width - margin, 250);
+    context.lineTo(margin, 50);
     context.stroke();
 }
