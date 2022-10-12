4fd0457f3ec455ee576a6bb3aea2f168ac81236a
Redmar Morales <redmarfrancis@yahoo.com>
2022-10-12 13:51:44 +0200

    Ex. 2 folder gemaakt

---
+++ exercises/2-variables/2-1-more-lines.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext('2d');
+
+let width = context.canvas.width;
+let height = context.canvas.height;
+
+drawLines();
+
+function drawLines() {
+    context.beginPath();
+
+    context.stroke();
+}
