8dd2048fa61495f0de49267b0121b73a82c77077
Redmar Morales <redmarfrancis@yahoo.com>
2022-11-17 19:34:31 +0100

    code aangepast

--- exercises/1-functions/1-6-starrynight.js
+++ exercises/1-functions/1-6-starrynight.js
@@ -8,18 +8,57 @@ let context = canvas.getContext('2d');
 drawStarrynight();
 
 function drawStarrynight() {
-    context.lineWidth = '5';
+    context.lineWidth = '10';
 
     //box
-    context.beginPath();
-    context.rect(50, 50, 350, 350);
-    context.stroke();
+    context.strokeStyle = "orange";
+    context.strokeRect(50, 50, 400, 400);
 
-    //circles
+    //star shape
+    context.fillStyle = "orange";
     context.beginPath();
-    context.stroke();
+    context.arc(50, 50, 200, 0, Math.PI / 3);
+    context.arc(50, 450, 200, Math.PI * 2 * 3 / 4, 0);
+    context.arc(450, 450, 200, Math.PI, Math.PI * 2 * 3 / 4);
+    context.arc(450, 50, 200, Math.PI / 2, Math.PI);
+    context.fill();
+
+    //center diamond shape
+    context.beginPath();
+    context.moveTo(250, 205);
+    context.lineTo(295, 250);
+    context.lineTo(250, 295);
+    context.lineTo(205, 250);
+    context.fill();
+
 
     //lines
+    context.strokeStyle = "white";
     context.beginPath();
+    context.moveTo(50, 50);
+    context.lineTo(450, 450);
+    context.moveTo(100, 50);
+    context.lineTo(450, 400);
+    context.moveTo(150, 50);
+    context.lineTo(450, 350);
+    context.moveTo(50, 100);
+    context.lineTo(400, 450);
+    context.moveTo(50, 150);
+    context.lineTo(350, 450);
+    context.moveTo(50, 450);
+    context.lineTo(450, 50);
+    context.moveTo(100, 450);
+    context.lineTo(450, 100);
+    context.moveTo(150, 450);
+    context.lineTo(450, 150);
+    context.moveTo(50, 400);
+    context.lineTo(400, 50);
+    context.moveTo(50, 350);
+    context.lineTo(350, 50);
     context.stroke();
+
+    //text
+    context.textAlign = "center";
+    context.font = "normal 40pt Arial";
+    context.fillText("STARRY NIGHT", 250, 500);
 }
